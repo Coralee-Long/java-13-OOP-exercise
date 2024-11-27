c058ea8e08bba85250245055116cc05b65e0b8b1
Coralee Long <coralee.long@gmail.com>
2024-11-27 11:23:33 +0100

    class and object exercise

---
+++ src/main/java/com/cora/Car.java
@@ -0,0 +1,29 @@
+package com.cora;
+
+public class Car extends Vehicle {
+
+    private int numDoors;
+
+    //constructor
+    public Car(String manufacturer, String model, int year, int numDoors) {
+        super(manufacturer, model, year);
+        this.numDoors = numDoors;
+    }
+
+    public int getNumDoors() {
+        return numDoors;
+    }
+    public void setNumDoors(int numDoors) {
+        this.numDoors = numDoors;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "numDoors=" + numDoors +
+                "manufacturer='" + getManufacturer() + '\'' +
+                ", model='" + getModel() + '\'' +
+                ", year=" + getYear() +
+                '}';
+    }
+}
--- src/main/java/com/cora/Main.java
+++ src/main/java/com/cora/Main.java
@@ -2,6 +2,13 @@ package com.cora;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+    Vehicle vehicle = new Vehicle("VW", "Passat", 2004);
+    Car car = new Car("BMW", "Cabrio", 2020, 4);
+
+        System.out.println(vehicle);
+        System.out.println(car);
+
+
     }
 }
